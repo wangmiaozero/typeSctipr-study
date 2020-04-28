264d2387af3c3712c2ebc5f93e1fe327f4280658
wangmiaozero <1061214467@qq.com>
2020-04-28 09:16:35 +0800

    1

---
+++ 基本类型合集.js
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: wangmiao
+ * @Date: 2020-04-28 09:03:14
+ * @LastEditors: wangmiao
+ * @LastEditTime: 2020-04-28 09:03:42
+ */
+// 数字，二、八、十六进制都支持
+var decLiteral = 6;
+console.log(decLiteral);
