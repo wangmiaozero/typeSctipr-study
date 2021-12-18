712567fe8b3b074e7ef7d9102ddbe30baf926064
wangmiao <1061214467@qq.com>
2021-12-18 22:56:37 +0800

    typescript

---
+++ practice_vue3_ts/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="todo-wrap">
+    <h2>todoList</h2>
+    <Header :addToPlan = 'addToPlan'></Header>
+    <List :todos = 'todos' ></List>
+    <Footer :todos ='todos'
+            :clearCompleted = 'clearCompleted'
+            :checkAll = 'checkAll'/>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent,provide,reactive,toRefs,watch,onMounted} from 'vue';
+import Header from './components/Head.vue'
+import List from './components/List.vue'
+import Footer from './components/Footer.vue'
+import {Todo} from "@/types/todo.ts";
+import {saveTodos,readTodos} from "@/utils/localStorageUtils";
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    // const state = reactive<{todos:Todo[]}>({
+    //   todos:[
+    //     {id:1,title:'xxx',isCompleted:false},
+    //     {id:2,title:'xxx',isCompleted:true},
+    //     {id:3,title:'xxx',isCompleted:false}
+    //   ]
+    // })
+    const state = reactive<{todos:Todo[]}>({
+      todos:[]
+    })
+    //界面加载完毕后再读取数据
+    onMounted(()=> {
+      setTimeout(()=>{  state.todos = readTodos()},500)
+
+    })
+    // value=>saveTodos(value)缩写
+    watch(()=>state.todos, saveTodos,{deep:true})
+
+
+    // 用于head组件添加数据
+    const addToPlan = (todo:Todo)=> {
+      state.todos.unshift(todo)
+    }
+    const delTodo:Function = (index:number)=> {
+      state.todos.splice(index,1)
+    }
+    const updateState = (todo:Todo,val:boolean)=> {
+      todo.isCompleted = val
+      console.log(todo)
+    }
+    const checkAll = (val:boolean)=> {
+      state.todos.forEach(item=>{
+        item.isCompleted = val
+      })
+    }
+    const clearCompleted = ()=> {
+      state.todos = state.todos.filter((item,index)=>{
+        if(item.isCompleted) {
+          return false
+        }
+        return true
+      })
+    }
+    provide('delTodo',delTodo)
+    provide('updateState',updateState)
+    return{
+      // 将state中的所有属性转换为ref对象 ，用的时候直接使用属性名称即可
+      ...toRefs(state),
+      // 给header组件的添加方法
+      addToPlan,
+      // 给footer组件的全选方法
+      checkAll,
+      clearCompleted
+    }
+  },
+  components: {
+    Header,List,Footer
+  }
+});
+</script>
+
+<style>
+
+  .todo-wrap {
+    width: 500px;
+    margin: 0 auto;
+    border: 1px solid lightblue;
+    border-radius: 5px;
+    padding: 20px;
+  }
+  .todo-wrap h2 {
+    text-align: center;
+  }
+</style>
---
+++ practice_vue3_ts/备份/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script lang="ts">
+  //这里可以使用ts的代码
+// defineComponent函数，目的是定义一个组件 , 内部可以传入一个配置对象
+import { defineComponent } from 'vue';
+// 引入一个子级组件
+import HelloWorld from './components/HelloWorld.vue';
+
+// 暴露出去一个定义好的组件
+export default defineComponent({
+  // 当前组件的名字是app
+  name: 'App',
+  // 注册组件
+  components: {
+    // 注册一个子级组件
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+
+</style>
